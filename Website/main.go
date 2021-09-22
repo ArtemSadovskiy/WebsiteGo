49dc877d3ddd6118d4f51b15996f17729591ad58
Artem Sadovskiy <artem8sadovskij@gmail.com>
2021-09-23 00:36:41 +0300

    add htmlAndCss

--- Website/main.go
+++ Website/main.go
@@ -2,29 +2,35 @@ package main
 
 import (
 	"fmt"
+	"html/template"
 	"net/http"
 )
 
 type User struct {
-	name                  string
-	age                   uint16
-	money                 int16
-	avg_grades, happiness float64
+	Name                  string
+	Age                   uint16
+	Money                 int16
+	Avg_grades, Happiness float64
+	Hobbies               []string
 }
 
 func (u *User) getAllInfo() string {
 	return fmt.Sprintf("User name is: %s. He is %d and he has money "+
-		"equal: %d", u.name, u.age, u.money)
+		"equal: %d", u.Name, u.Age, u.Money)
 }
 
 func (u *User) setNewName(newName string) {
-	u.name = newName
+	u.Name = newName
 }
 
 func home_page(w http.ResponseWriter, r *http.Request) {
-	bob := User{"Bob", 25, 100, 4.5, 0.8}
-	bob.setNewName("Alex")
-	fmt.Fprintf(w, bob.getAllInfo())
+	bob := User{"Bob", 25, 100, 4.5, 0.8, []string{"Football", "Skate", "Dance"}}
+	//bob.setNewName("Alex")
+	//fmt.Fprintf(w, bob.getAllInfo())
+	// fmt.Fprintf(w, `<h1>Main Text</h1>
+	// 	<b>Main Text</b>`)
+	tmpl, _ := template.ParseFiles("templates/home_page.html")
+	tmpl.Execute(w, bob)
 }
 
 func contacts_page(w http.ResponseWriter, r *http.Request) {
@@ -38,6 +44,6 @@ func handleRequest() {
 }
 
 func main() {
-	//bob := User{name: "Bob", age: 25, money: -50, avg_grades: 4.5, happiness: 0.8}
+	//bob := User{Name: "Bob", Age: 25, Money: -50, Avg_grades: 4.5, Happiness: 0.8}
 	handleRequest()
 }
